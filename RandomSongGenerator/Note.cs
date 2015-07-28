ba7f13c9d6172964567f0169c9985fb7b4d575b6
Mike <mfriedmanisgod@netscape.net>
2015-07-28 14:59:27 -0400

    still working on my scale array. its actually an array of tone arrays. come at it from a different angle.

--- RandomSongGenerator/Note.cs
+++ RandomSongGenerator/Note.cs
@@ -27,36 +27,25 @@ namespace RandomSongGenerator
         public Duration NoteDuration { get { return durVal; } }
     }
 
-    public struct NoteCollection
+    public struct NoteArray
     {
-        public Note[] noteCollection { get; set; }
+        public Scale[] scaleCollection { get; set; }
         
-        public static Note[] GetAminorSeptatonicScale()
+        public Note[] GetAminorArray()
         {
-            Note[] septatonicScale1 = new Note[];
             
-            septatonicScale1.toneCollection = new Tone[8];
-            septatonicScale1.toneCollection[0] = Tone.A;
-            septatonicScale1.toneCollection[1] = Tone.B;
-            septatonicScale1.toneCollection[2] = Tone.C;
-            septatonicScale1.toneCollection[3] = Tone.D; 
-            septatonicScale1.toneCollection[4] = Tone.E;
-            septatonicScale1.toneCollection[5] = Tone.F;
-            septatonicScale1.toneCollection[6] = Tone.G;
-            septatonicScale1.toneCollection[7] = Tone.A2;
+            Note[] noteArray = new Note[5];  
             
-            return septatonicScale1;
+            
+         
+
+            
+            
+            
+            return noteArray;
         }
 
-        Tone[] scale = new Tone;
-        Note note1 = new Note(scale, Duration.QUARTER);
-        Note note2 = new Note(a_Aeolien.noteCollection[1], Duration.QUARTER);
-        Note note3 = new Note(a_Aeolien.noteCollection[2], Duration.QUARTER);
-        Note note4 = new Note(a_Aeolien.noteCollection[3], Duration.QUARTER);
-        Note note5 = new Note(a_Aeolien.noteCollection[4], Duration.QUARTER);
-        Note note6 = new Note(a_Aeolien.noteCollection[5], Duration.QUARTER);
-        Note note7 = new Note(a_Aeolien.noteCollection[6], Duration.QUARTER);
-        Note note8 = new Note(a_Aeolien.noteCollection[7], Duration.QUARTER);
+        
 
         /*        
         audioPlayer.Play(note1, (int)tempoInput.Value);
